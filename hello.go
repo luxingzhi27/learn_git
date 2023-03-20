22801e8a5283fa2c151e0e7388de34ba195c3252
luxingzhi27 <3523718637@qq.com>
2023-03-20 16:15:18 +0800

    add sum function

--- hello.go
+++ hello.go
@@ -2,4 +2,12 @@ package main
 
 func main(){
   fmt.Println("hello world")
+  fmt.Println(sum(1,2,3,4,5))
+}
+
+func sum(args ...int) int{
+    ans:=0
+    for _,val:range args{
+        ans+=val
+    }
 }
