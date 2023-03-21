d25e1ae2ebde5fea567d21a4e3ae942df04e72d9
luxingzhi27 <3523718637@qq.com>
2023-03-21 10:05:24 +0800

    hello

--- hello.go
+++ hello.go
@@ -1,14 +1,18 @@
 package main
 
-func main(){
-  fmt.Println("hello world")
-  fmt.Println(sum(3536738,77,38))
-  fmt.Println(sum(1,2,3,4,5))
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(sum(3536738, 77, 38))
+	fmt.Println(sum(1, 2, 3, 4, 5))
+	fmt.Println(sum(3, 3, 322, 22, 444, 99))
 }
 
-func sum(args ...int) int{
-    ans:=0
-    for _,val:range args{
-        ans+=val
-    }
+func sum(args ...int) int {
+	ans := 0
+	for _, val := range args {
+		ans += val
+	}
+	return ans
 }
